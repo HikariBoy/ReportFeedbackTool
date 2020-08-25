daa3cf0503cd19ab51b74928bf59bfc86c4f11c5
Adrian <adrian.keating@uwa.edu.au>
2020-08-25 14:53:22 +0800

    added Error Messages to output and color
    Messageboxes added Re Config File

--- grading_gui.py
+++ grading_gui.py
@@ -8,22 +8,41 @@ Created on Mon Apr 20 18:43:39 2020
 #!/usr/bin/python3
 from ctypes import *
 import tkinter as tk
+from tkinter import messagebox
+#import tkinter.messagebox as  messagebox
 import tkinter.font as tkFont
 from tkinter.ttk import Progressbar
 import yaml
+import sys,os
  
 
 __author__="Adrian Keating(UWA)"
 __version__ = "0.1.1"
 selections=[]
 
+
+class MyError(tk.Toplevel):
+    def __init__(self, message):
+        tk.Toplevel.__init__(self)
+        tk.Label(self, text=message).grid(row=0, column=0)
+        tk.Button(self, command=self.destroy, text="OK").grid(row=1, column=0)
+        self.lift()  # Puts Window on top
+        self.grab_set()  # Prevents other Tkinter windows from being used
+
+
+def showerror(string):
+    MyError(string)
+    
+class ConfigError(Exception):
+    pass
+
 def allstates():
        print(list(lng.state()) )
 
 
 class Checkbar(tk.Frame):
-   def __init__(self, parent=None, picks=[],side="left", anchor="w", background='white'):
-      tk.Frame.__init__(self, parent,background='white', )
+   def __init__(self, parent=None, picks=[],side="left", anchor="w", background='light yellow'):
+      tk.Frame.__init__(self, parent,background='light yellow', )
       self.Frame1=tk.Frame
       self.vars = []
       self.listpicks=picks[::-1]  # COPIES and reverses the list and stores
@@ -80,7 +99,7 @@ class Checkbar(tk.Frame):
              starti=endi
          #if(endi>0):
          #    newpick=newpick[0:-1]
-         chk = tk.Checkbutton(self, borderwidth=3,text=newpick, variable=var,background='white',justify = "left")
+         chk = tk.Checkbutton(self, borderwidth=3,text=newpick, variable=var,background='light yellow',justify = "left")
          chk.pack( anchor='w')
          self.check_box_list.append(chk)
          self.vars.append(var)
@@ -95,14 +114,14 @@ class Checkbar(tk.Frame):
 class RadioFrame(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         ###call the parent constructor
-        tk.Frame.__init__(self, parent,bg="white", *args, **kwargs)
+        tk.Frame.__init__(self, parent,bg="light blue", *args, **kwargs)
         self.parent = parent
         self.pack(fill="both", side="top",expand=True)
         N=len(Rubric)
         M=int(N/3)+1
         frame=[0]*M
         for indx in range(M):
-            frame[indx]=tk.Frame(self,bg="white",bd=0,width=0)
+            frame[indx]=tk.Frame(self,bg="light blue",bd=0,width=0)
             frame[indx].pack(fill="both",expand=True)
         frame1=tk.Frame(self,bg="white",bd=0,width=0) 
         frame2=tk.Frame(self,bg="white",bd=0,width=0)
@@ -117,6 +136,7 @@ class RadioFrame(tk.Frame):
             tk.Radiobutton(frame[indx], 
                           text=language,
                           padx = 1, 
+                          bg="light blue",
                           variable=v, 
                           command=ShowChoice,
                           value=val).pack(side="left")
@@ -142,12 +162,18 @@ class StartEndAppFrame(tk.Frame):
         bd5scaled=int(5*scale)
         bd12scaled=int(8*scale)
         width20scaled=int(20*scale)
-        width5scaled=int(scale*10)
-        self.btn = tk.Button(frame1, text='Accept&Copy',bd=bd12scaled, width=width5scaled*2,height=int(scale), relief="raised",command=setText)
+        width5scaled=int(fontsize*.8)
+        scaleheight=max(int(fontsize/9),1)
+        #print("scale",scale)
+        #print("lenaccept",len('Accept&Copy'))
+        #print("lenacopy",len('copy'))
+        self.btn =createButton('Accept&Copy',frame1,setText)
+        #self.btn = tk.Button(frame1, text='Accept&Copy',bd=bd12scaled, width=11,height=scaleheight, relief="raised",command=setText)
         self.btn.pack({"side": "left"},expand=True)
         self.btn.pack(pady=1)
         #print(hlpbtnimg)
-        btn1b = tk.Button(frame1, text='Copy',bd=bd12scaled, width=width5scaled,height=int(scale), relief="raised",command=CopyText)
+        btn1b =createButton('Copy',frame1,CopyText)
+        #btn1b = tk.Button(frame1, text='Copy',bd=bd12scaled, width=4,height=scaleheight, relief="raised",command=CopyText)
         #btn2.pack(weight=1)
         btn1b.pack({"side": "left"},expand=True)
         btn1b.pack(pady=1)
@@ -155,7 +181,8 @@ class StartEndAppFrame(tk.Frame):
         #hlpbtn.pack({"side": "left"},expand=True)
         #hlpbtn.pack(pady=5)
                 ###Create button
-        btn2 = tk.Button(frame1, text='EXIT',bd=bd12scaled, width=width5scaled,height=int(scale), relief="raised",command=gracefullexit)
+        btn2=createButton('EXIT',frame1,gracefullexit,mybg="pink")
+        #btn2 = tk.Button(frame1, text='EXIT',bg="pink",bd=bd12scaled, width=width5scaled,height=scaleheight, relief="raised",command=gracefullexit)
         #btn2.pack(weight=1)
         btn2.pack({"side": "right"},expand=True)
         btn2.pack(pady=1)
@@ -164,7 +191,12 @@ class StartEndAppFrame(tk.Frame):
         #return
 
 
-
+def createButton(btn_name,frame, function,mybg='light grey'):
+    w=len(btn_name)+2
+    bdscaled=int(fontsize)
+    scaleheight=max(int(fontsize/9),1)
+    btn = tk.Button(frame, bg=mybg,text=btn_name,bd=bdscaled, width=w,height=scaleheight, relief="raised",command=function)
+    return btn
 
     def increament(self):
         ii=GradingProgress.progress_var.get()
@@ -211,7 +243,14 @@ def CopyText():
 def ShowChoice():
     selections=GradingConfigData['constants'][Rubric[v.get()]]
     lng.updatelist(selections)
- 
+
+def mymessagebox(title, text):
+    localroot = tk.Tk()
+    localroot.overrideredirect(1)
+    localroot.withdraw()
+    messagebox.showinfo(title, text)
+    localroot.destroy()
+    
 if __name__ == '__main__':
    windll.shcore.SetProcessDpiAwareness(c_int(1))
    if getattr(sys, 'frozen', False): # Running as compiled
@@ -220,64 +259,125 @@ if __name__ == '__main__':
    else:
        running_dir = r'.\\' # Path name when run with Python interpreter
    
+   root = tk.Tk()
+   #root.overrideredirect(1) # To avoid a "flash" as the root window is created, use this slight variation on the accepted answer
    configfile='configList.yaml'
-   with open(configfile,'r', encoding='utf8') as f:
+   
+   #texto = tk.Toplevel(root)
+   #showerror("Help!")
+   try:
+       with open(configfile,'r', encoding='utf8') as f:
         # utf-8 required to load accented text such as André-Marie Ampère
-        try:
+        #try:
             # with open(config, 'rt', encoding='utf8') as yml:
             # config_obj = yaml.load(yml)
             GradingConfigData = yaml.load(f, Loader=yaml.FullLoader)
             for eachvariable in  (GradingConfigData['constants'].keys()):
+                print(eachvariable)
                 locals()[eachvariable]=GradingConfigData['constants'][eachvariable]
 
-        except (NameError) as error:
-            print('A key in the file '+configfile+' is not understood')
-            raise
-        except:
-            raise
-            
-   selections=GradingConfigData['constants'][Rubric[0]]
 
 
-   if getattr(sys, 'frozen', False): # Running as compiled
-       running_dir = sys._MEIPASS + '\\'
-       # Same path name than pyinstaller option
-   else:
-       running_dir = r'.\\' # Path name when run with Python interpreter
-   root = tk.Tk()
-   v = tk.IntVar()
 
-   root.after(0, root.focus_force())
-   root.wm_attributes('-fullscreen', 'False')
-   root.call('wm', 'attributes', '.', '-topmost', '1')
+       for indx in range(len(Rubric)):
+         # check each value exists
+         GradingConfigData['constants'][Rubric[indx]]
 
-   if (1):
-       Wmain=root.winfo_screenwidth()
-       Hmain=root.winfo_screenheight()
-       scale=1*Hmain/1800
-       w=int(200*scale)
-       h=int(660*w/1100)
-       fontsize=int(9*scale)
-       root.configure(width=w, height=h)  #root.geometry("300x200+300+300")
-       Magnify=10
-       root.option_add("*Font", "helvetica "+str(fontsize)+" bold")
-       root.title('Grading Tool ('+str(__version__)+') - By Dr. Adrian Keating (C) UWA, 2020')
-       root.configure(background='white')
-       radios=RadioFrame(root)
-       GoQuit=StartEndAppFrame(root)
-       GoQuit.pack(side="top")
-       if(1):
-           lng = Checkbar(root, selections )
-           #print(lng.state())
-           lng.state() #tgl = Checkbar(root, ['English','German'])
-           lng.pack(side="top",  fill="x") #(fill="both", expand=True)
-           #tgl.pack(side="left")
-           bd2scaled=int(2*scale)
-           lng.config(relief="groove", bd=bd2scaled)
-       if(1):
-           print(w)
-           SearchParams = tk.Text(root,wrap=tk.WORD,width=int(w/4),height=10)
-           SearchParams.pack({"side": "top"})
-       root.lift()
-       #root.update()
-   root.mainloop()
+       
+       #raise ConfigError(ErrorMsg)
+       #sys.exit(0)
+       #exit(1)         
+       selections=GradingConfigData['constants'][Rubric[0]]
+    
+    
+       if getattr(sys, 'frozen', False): # Running as compiled
+           running_dir = sys._MEIPASS + '\\'
+           # Same path name than pyinstaller option
+       else:
+           running_dir = r'.\\' # Path name when run with Python interpreter
+    
+       v = tk.IntVar()
+    
+       root.after(0, root.focus_force())
+       root.wm_attributes('-fullscreen', 'False')
+       root.call('wm', 'attributes', '.', '-topmost', '1')
+    
+       if (1):
+           Wmain=root.winfo_screenwidth()
+           Hmain=root.winfo_screenheight()
+           print(Wmain,Hmain)
+           
+    
+           # Get the reported DPI from the window's HWND
+           dpi = windll.user32.GetDpiForWindow(root.winfo_id())
+           # Print the DPI
+           print('dpi',dpi/96)
+    
+           scale=1*Hmain*2/((dpi/96)*1800)
+           #scale=1*Hmain/(1800)
+           w=int(150*scale)
+           h=int(660*w/1100)
+           fontsize=int(9*scale)
+           print("fontsize",fontsize)
+           root.configure(width=w, height=h)  #root.geometry("300x200+300+300") app.geometry("300x100")
+           Magnify=10
+           root.option_add("*Font", "helvetica "+str(int(fontsize))+" bold")
+           root.title('Grading Tool ('+str(__version__)+')/Dr. A. Keating, UWA, 2020')
+           root.configure(background='white')
+           radios=RadioFrame(root)
+           GoQuit=StartEndAppFrame(root)
+           GoQuit.pack(side="top")
+           if(1):
+               lng = Checkbar(root, selections )
+               #print(lng.state())
+               lng.state() #tgl = Checkbar(root, ['English','German'])
+               lng.pack(side="top",  fill="x") #(fill="both", expand=True)
+               #tgl.pack(side="left")
+               bd2scaled=int(2*scale)
+               lng.config(relief="groove", bd=bd2scaled)
+           if(1):
+               print(w)
+               SearchParams = tk.Text(root,wrap=tk.WORD,width=int(w/4),height=10)
+               SearchParams.pack({"side": "top"})
+           root.lift()
+           #root.update()
+       root.mainloop()
+   except (NameError) as error:
+        #raise ConfigError('A key in the file '+configfile+' is not understood. Please edit the file, checking spelling and format')
+        ErrorMsg='A key in the file '+configfile+' is not understood. Please edit the file, checking spelling and format'
+        #print('A key in the file '+configfile+' is not understood')
+        #sys.exit(os.EX_OK)
+        root.withdraw() # don't show root window
+        root.attributes('-topmost', True)
+        messagebox.showinfo("CONFIG FILE ERROR", ErrorMsg)
+        gracefullexit()
+   except (FileNotFoundError) as error:
+        #raise ConfigError('The File '+configfile+' Could not be found.  Check it has been placed in teh same directory as the main program.')
+        #print('A key in the file '+configfile+' is not understood')
+        ErrorMsg='The File '+configfile+' Could not be found.  Check it has been placed in the same directory as the main program.'
+        root.withdraw() # don't show root window
+        root.attributes('-topmost', True)
+        messagebox.showinfo("CONFIG FILE ERROR", ErrorMsg)
+        gracefullexit()
+   except Exception as e:
+        #raise ConfigError('An Error occurred when reading the file: '+configfile +'\nSpecific Error is:'+e)
+        ErrorMsg='An Error occurred when reading the file: '+configfile +'\nSpecific Error for unknow parameter is: '+str(e)
+
+        #texto = tk.Toplevel(root)
+        #texto.withdraw()
+        #texto.attributes('-topmost', True) # don't use topmost command
+        root.withdraw() # don't show root window
+        root.attributes('-topmost', True)
+        messagebox.showinfo("CONFIG FILE ERROR", ErrorMsg)
+        #texto.destroy()
+        #messagebox.showinfo("CONFIG FILE ERROR", ErrorMsg)
+        gracefullexit()
+        
+   except (KeyError) as e:
+        ErrorMsg='The unknown parameter is :'+str(e)
+        ErrorMsg='Unknown value in the file: '+configfile +'. Check spelling and format\n'+ErrorMsg
+        #showerror(ErrorMsg) 
+        root.withdraw() # don't show root window
+        root.attributes('-topmost', True)
+        messagebox.showinfo("CONFIG FILE ERROR", ErrorMsg)
+        gracefullexit()
